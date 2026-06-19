865c63a1b55b43ca435b234c1484e9c19d2459e5
JACKDANIELS-777 <78420661+JACKDANIELS-777@users.noreply.github.com>
2026-06-19 11:53:19 +0200

    Update Dasm.asm

--- Code/Versions/V0.04/Dasm.asm
+++ Code/Versions/V0.04/Dasm.asm
@@ -1,3 +1,12 @@
 rax=^:rbx
 rax=^:([0]<-rbx)
 rax=v:([0]->rbx)
+
+;Intended Disassembly
+;Disassembly:
+;0:  53                      push   rbx
+;1:  48 89 d8                mov    rax,rbx
+;4:  48 89 5c 24 00          mov    QWORD PTR [rsp+0x0],rbx
+;9:  48 89 d8                mov    rax,rbx
+;c:  48 8b 5c 24 00          mov    rbx,QWORD PTR [rsp+0x0]
+;11: 48 89 d8                mov    rax,rbx
